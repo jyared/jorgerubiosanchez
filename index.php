7a4daa388e65c35eed3932886cc9a1daf26090dc
jyared <R_E_D_D@hotmail.com>
2023-04-12 00:22:20 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<!Doctype html>
+<head>
+<title>prueba</title>
+<link href="css/bootstrap.min.css" rel="stylesheet">
+<link href="css/propios.css?V1" rel="stylesheet">
+</head>
+<body class="container">
+
+<br><br><br><br>
+<div id="menu">
+<ul class="nav nav-tabs"> 
+   <li class="active"><a href="#" onclick="formularioLogin()">Ingresar</a></li> 
+<!-- <li><a href="#" onclick="solicitarRegistroColaborador()">REGISTRO COLABORADOR</a></li> 
+   <li><a href="#" onclick='solicitarAsignacionCuestionario()'>ASIGNAR CUESTIONARIO</a></li> 
+   <li><a href="#" id="crear">CREAR</a></li> 
+    <li><a href="#" id="registrarExamen" onclick="dibujarFormCuestionario()">REGISTRAR EXAMEN</a></li> 
+-->
+</ul>
+</div>
+
+<div class="span12"id="principal" class="formulario">
+
+<br><br><br><br><br>
+
+</div>
+
+
+
+
+<script src="js/jquery.js" type="text/javascript"></script>
+<script src="js/funciones.js?v1" type="text/javascript"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.15.3/xlsx.full.min.js"></script>
+
+</body>
+</html>
